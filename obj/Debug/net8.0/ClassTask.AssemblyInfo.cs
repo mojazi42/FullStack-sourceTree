99aa578ec6faabd761c2f844d569c799e65b20d8
LAPTOP-JL720CH5\moh20 <mojazi42@gmail.com>
2025-03-03 02:06:39 +0300

    DB&ASP.NET

--- obj/Debug/net8.0/ClassTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClassTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b2caeec501bf9ee225942e72d55074836a361b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
